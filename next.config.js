161887bf838190f86feb621023e1c21176c09e1b
Hugo Cardoso <hugorafaelmc@gmail.com>
2022-06-28 21:44:39 -0300

    Added PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
   reactStrictMode: true,
   async redirects() {
-    return [{
-       source: '/app',
-       destination: '/app/search',
-       permanent: true,
-    }];
- },
-}
+    return [
+      {
+        source: "/app",
+        destination: "/app/search",
+        permanent: true,
+      },
+    ];
+  },
+  pwa: {
+    dest: "public",
+		runtimeCaching,
+  },
+});
