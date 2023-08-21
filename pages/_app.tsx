750433244382b9c165b25b7ecbe718e408e975e7
Hugo Cardoso <hugorafaelmc@gmail.com>
2023-08-21 10:22:31 -0300

    feat: adicionado microsoft clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,6 +41,15 @@ function MyApp({ Component, pageProps }: AppProps) {
                 gtag('config', 'G-KSPD90VK7W');
               `}
             </Script>
+            <Script id="microsoft-clarity" strategy="afterInteractive">
+              {`
+                (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "iik2r8sxg6");
+              `}
+            </Script>
           </>
         )
       }
