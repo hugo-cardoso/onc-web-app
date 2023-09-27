1b38a2b078e0076d3669802404529f6201eeca06
Hugo Cardoso <hugorafaelmc@gmail.com>
2023-09-26 21:20:21 -0300

    chore: alterado url das cartas

--- src/components/ProcedureViewer/index.tsx
+++ src/components/ProcedureViewer/index.tsx
@@ -175,7 +175,7 @@ export const ProcedureViewer = (props: ProcedureViewerProps) => {
     <Styles.Layout isFull={!procedureViewerContext.toolbarIsOpen}>
       <Styles.Wrapper ref={wrapperRef}>
         <Document
-          file={`https://onc-charts.s3.sa-east-1.amazonaws.com/${props.procedure.icao}/${props.procedure.id}.pdf`}
+          file={`https://d3k987dq9enre4.cloudfront.net/${props.procedure.icao}/${props.procedure.id}.pdf`}
           renderMode='canvas'
           onLoadSuccess={handleDocumentLoadSuccess}
           onLoadError={() => setStatus('error')}
